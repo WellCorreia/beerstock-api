244348a53ba97b11826aa1e7fd80f74435462ce9
Wellington Correia <wellington.dossantos@montreal.com.br>
2021-03-13 17:56:55 -0300

    Developed tests of controller and service.

--- src/test/java/dev/wellington/beerstock/service/BeerServiceTest.java
+++ src/test/java/dev/wellington/beerstock/service/BeerServiceTest.java
@@ -8,10 +8,13 @@ import dev.wellington.beerstock.exception.BeerNotFoundException;
 import dev.wellington.beerstock.exception.BeerStockExceededException;
 import dev.wellington.beerstock.mapper.BeerMapper;
 import dev.wellington.beerstock.repository.BeerRepository;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
@@ -33,5 +36,89 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BeerServiceTest {
+    private static final long INVALID_BEER_ID = 1L;
+
+    @Mock
+    private BeerRepository beerRepository;
+
+    private BeerMapper beerMapper = BeerMapper.INSTANCE;
+
+    @InjectMocks
+    private BeerService beerService;
+
+    @Test
+    void whenBeerInformedThenItShoutBeCreated() throws BeerAlreadyRegisteredException {
+        //given
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer expectedBeer = beerMapper.toModel(beerDTO);
+
+        //when
+        Mockito.when(beerRepository.findByName(beerDTO.getName())).thenReturn(Optional.empty());
+        Mockito.when(beerRepository.save(expectedBeer)).thenReturn(expectedBeer);
+
+        //then
+        BeerDTO createdBeerDTO = beerService.createBeer(beerDTO);
+
+        //Matcher of Hamcrest
+        MatcherAssert.assertThat(createdBeerDTO.getId(), Matchers.is(Matchers.equalTo(beerDTO.getId())));
+        MatcherAssert.assertThat(createdBeerDTO.getName(), Matchers.is(Matchers.equalTo(beerDTO.getName())));
+        MatcherAssert.assertThat(createdBeerDTO.getQuantity(), Matchers.is(Matchers.equalTo(beerDTO.getQuantity())));
+
+        //Matcher of Junit
+        //        assertEquals(beerDTO.getId(), createdBeerDTO.getId());
+        //        assertEquals(beerDTO.getName(), createdBeerDTO.getName());
+    }
+    @Test
+    void whenAlreadyRegisteredInformedThenAnExeptionShouldBeThrown() throws BeerAlreadyRegisteredException {
+        //given
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer duplicatedBeer = beerMapper.toModel(beerDTO);
+
+        //when
+        Mockito.when(beerRepository.findByName(beerDTO.getName())).thenReturn(Optional.of(duplicatedBeer));
+
+        //then
+        assertThrows(BeerAlreadyRegisteredException.class, () -> beerService.createBeer(beerDTO));
+    }
+    @Test
+    void whenListBeerIsCalledThenReturnAListOfBeers() {
+        //given
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer expectedFoundBeer = beerMapper.toModel(beerDTO);
+
+        //when
+        Mockito.when(beerRepository.findAll()).thenReturn(Collections.singletonList(expectedFoundBeer));
+
+        //then
+        List<BeerDTO> foundListBeersDTO = beerService.listAll();
+        MatcherAssert.assertThat(foundListBeersDTO, is(not(empty())));
+        MatcherAssert.assertThat(foundListBeersDTO.get(0), is(equalTo(beerDTO)));
+    }
+
+    @Test
+    void whenListBeerIsCalledThenReturnAnEmptyListOfBeers() {
+        //when
+        Mockito.when(beerRepository.findAll()).thenReturn(Collections.EMPTY_LIST);
+
+        //then
+        List<BeerDTO> foundListBeersDTO = beerService.listAll();
+        MatcherAssert.assertThat(foundListBeersDTO, is(empty()));
+    }
+    @Test
+    void whenDeleteBeerIsCalledThenABeerShouldBeDelete() throws BeerNotFoundException {
+        //given
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer expectedDeleteBeer = beerMapper.toModel(beerDTO);
+
+        //when
+        Mockito.when(beerRepository.findById(expectedDeleteBeer.getId())).thenReturn(Optional.of(expectedDeleteBeer));
+        doNothing().when(beerRepository).deleteById(beerDTO.getId());
+
+        //then
+        beerService.deleteById(beerDTO.getId());
+
+        verify(beerRepository, times(1)).findById(beerDTO.getId());
+        verify(beerRepository, times(1)).deleteById(beerDTO.getId());
+    }
 
 }
