357277a093b5c78467bdbcaf391dc0d06895ef2d
Wellington Correia <wellington.dossantos@montreal.com.br>
2021-03-13 12:25:48 -0300

    Add base project.

---
+++ src/main/java/dev/wellington/beerstock/mapper/BeerMapper.java
@@ -0,0 +1,16 @@
+package dev.wellington.beerstock.mapper;
+
+import dev.wellington.beerstock.dto.BeerDTO;
+import dev.wellington.beerstock.entity.Beer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+
+    Beer toModel(BeerDTO beerDTO);
+
+    BeerDTO toDTO(Beer beer);
+}
---
+++ src/main/java/dev/wellington/beerstock/repository/BeerRepository.java
@@ -0,0 +1,11 @@
+package dev.wellington.beerstock.repository;
+
+import dev.wellington.beerstock.entity.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
+}
